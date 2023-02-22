189db8676ce87b5b1ca434d5f21e5a1407394359
James McNider <james@Jamess-MacBook-Air.local>
2023-02-21 21:40:20 -0500

    done with tutorial

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,7 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
-  "sk_test_51HPvU9DFg5koCdLGeOEiFvwHat4v8eMjX6SY0YCwxPBQBUPhKy1fPVhiSM5cQtgW7QBG9ydQcXnW57TDxVE2f3H000HSfmEQZF"
-);
+  "pk_live_xCuYtXBX78Psbe8z0uPmHTC5");
 
 // API
 
@@ -37,4 +36,4 @@ app.post("/payments/create", async (request, response) => {
 exports.api = functions.https.onRequest(app);
 
 // Example endpoint
-// http://localhost:5001/challenge-4b2b2/us-central1/api
+// http://127.0.0.1:5001/spaceymarket/us-central1/api
--- src/Header.js
+++ src/Header.js
@@ -20,7 +20,7 @@ function Header() {
       <Link to="/">
         <img
           className="header__logo"
-          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          src={require("./logowhite.png")}
           alt=''
         />
       </Link>
@@ -33,22 +33,20 @@ function Header() {
       <div className="header__nav">
         <Link to={!user && '/login'}>
           <div onClick={handleAuthenticaton} className="header__option">
-            <span className="header__optionLineOne">Hello {!user ? 'Guest' : user.email}</span>
             <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
+            <span className="header__optionLineOne">{!user ? 'Guest' : user.email}</span>
           </div>
         </Link>
 
         <Link to='/orders'>
           <div className="header__option">
-            <span className="header__optionLineOne">Returns</span>
-            <span className="header__optionLineTwo">& Orders</span>
+            <span className="header__optionLineTwo">Orders</span>
           </div>
         </Link>
         
 
         <div className="header__option">
-          <span className="header__optionLineOne">Your</span>
-          <span className="header__optionLineTwo">Prime</span>
+          <span className="header__optionLineTwo">Account</span>
         </div>
 
         <Link to="/checkout">
